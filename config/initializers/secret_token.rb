a3c05b17314e709cee2f72e3e729326dae2ee17f
abello966 <antonio.augusto.abello@gmail.com>
2015-10-07 07:58:58 -0300

    real rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Progrules::Application.config.secret_token = '537099bd9bd9e60ed9caccca48ac165332b9bf77877d4fb178d9550d98ebd802df4d1390853b3833bf1f70f9bf2ad5e17212291f6927615c16394980e3e67921'
