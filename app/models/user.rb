1d0f6490f432f6c7e826d3192321e8e985be77c3
abello966 <antonio.augusto.abello@gmail.com>
2015-10-10 04:11:50 -0300

    mudanças finais model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ require 'digest' #funcao de hash
 
 class User < ActiveRecord::Base
   attr_accessible :name, :pass_hash, :position
+  belongs_to :aluno
   #nome, hash de senha, tipo (admin/aluno)
 end
