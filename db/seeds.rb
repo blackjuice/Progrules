2b3ed34758bd8dd06078558785c0c4edd9a3d40f
abello966 <antonio.augusto.abello@gmail.com>
2015-10-09 22:53:45 -0300

    sistema de login pronto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    first = User.create([{name: 'abello' }, {pass_hash: 'a320480f534776bddb5cdb54b1e93d210a3c7d199e80a23c1b2178497b184c76'}, {position: 'admin'}])
+
