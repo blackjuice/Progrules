b235f7156ec0f1b25f584dce28d11b143e2569c4
leodaneu <leonardodaneu93@gmail.com>
2015-12-10 14:55:43 -0400

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,7 @@
 # files.
 
 require 'cucumber/rails'
-Capybara.default_max_wait_time = 5
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
